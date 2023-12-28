0a82888de60b7ab4f4690a183acbacb04850fd43
wallaceferreira149 <wallaceferreira149@gmail.com>
2023-12-28 17:54:07 -0400

    build: :rocket: finish tutorial

---
+++ app/dashboard/invoices/[id]/edit/not-found.tsx
@@ -0,0 +1,17 @@
+import { FaceFrownIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+
+export default function NotFound(){
+  return (
+    <main className="flex h-full flex-col items-center justify-center gap-2">
+      <FaceFrownIcon className="w-10 text-gray-400" />
+      <h2 className="text-xl font-semibold">404 Not found</h2>
+      <p>Could not found the requested invoice.</p>
+      <Link href='/dashboard/invoices'
+        className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-white text-sm transition-colors hover:bg-blue-400"
+      >
+        Go back
+      </Link>
+    </main>
+  )
+}
